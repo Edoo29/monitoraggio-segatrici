435066e085853d6a2128cb092bde57d0343bb100
Edoo29 <edodev29@gmail.com>
2026-06-18 09:58:18 +0200

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router";
+
 import App from "./pages/App.jsx";
 import Setup from "./pages/Setup.jsx";
 import TitleBar from "./components/ui/TitleBar.jsx";
 
 const isMacOS = window.env?.platform === "darwin";
+const Router = window.electron ? HashRouter : BrowserRouter;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {!isMacOS && <TitleBar />}
-    <BrowserRouter>
+
+    <Router>
       <Routes>
         <Route path="/" element={<Setup />} />
         <Route path="/dashboard" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </StrictMode>,
 );
